64dbb0c25b22ab1d4a589d72eb132a6f97dd7811
DUMESTRE LEON p2006532 <leon.dumestre@etu.univ-lyon1.fr>
2022-02-08 15:34:57 +0100

    TP3 - Création et remplissage de la database

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -17,52 +17,80 @@ class ImagesController extends AppController
          return $this->response->withStringBody(json_encode($dataArray))->withType("application/json");
     }
 
-    //TODO Ajouter le numéro de page en paramètre
-    public function listing($name = null)
+    public function add()
+    {
+        $images = $this->Images
+            ->find()
+            ->all();
+
+        if (count($images) == 0) {
+            foreach (glob(WWW_ROOT . 'img/*.jpg') as $img) {
+                $image = $this->Images->newEmptyEntity();
+                $image->name = exif_read_data($img)["FileName"];
+
+                $description = '';
+                if (isset(exif_read_data($img)["ImageDescription"]))
+                    $description = exif_read_data($img)["ImageDescription"];
+                $image->description = $description;
+
+                $image->width = exif_read_data($img)["ExifImageWidth"];
+                $image->height = exif_read_data($img)["ExifImageLength"];
+
+                if ($this->Images->save($image))
+                    $id = $image->id;
+            }
+        }
+        dd('add');
+    }
+
+    //TODO pagination à faire
+    public function listing($args = null)
     {
         $request = $this->getRequest()->getQuery();
         $dataArray = array();
+
         foreach (glob(WWW_ROOT.'img/*.jpg') as $img) {
 
-            if (exif_read_data($img)["FileName"] == $name ||
-                substr(exif_read_data($img)["FileName"], 0, strpos(exif_read_data($img)["FileName"], '.')) == $name ||
-                $name == null)
-            {
-                if ((isset($request["limit"]) && count($dataArray) < $request["limit"]) || !isset($request["limit"])) {
-                    if ((isset($request["name"]) && str_contains(exif_read_data($img)["FileName"], $request["name"])) || !isset($request["name"])) {
+            if (count($dataArray) <= 10) {
 
-                        $description = '';
-                        if (isset(exif_read_data($img)["ImageDescription"]))
-                            $description = exif_read_data($img)["ImageDescription"];
+                if (exif_read_data($img)["FileName"] == $args ||
+                    substr(exif_read_data($img)["FileName"], 0, strpos(exif_read_data($img)["FileName"], '.')) == $args ||
+                    $args == null) {
 
-                        $comment = '';
-                        if (isset(exif_read_data($img)["UserComment"]))
-                            $comment = exif_read_data($img)["UserComment"];
+                    if ((isset($request["limit"]) && count($dataArray) < $request["limit"]) || !isset($request["limit"])) {
+                        if ((isset($request["name"]) && str_contains(exif_read_data($img)["FileName"], $request["name"])) || !isset($request["name"])) {
 
-                        $author = '';
-                        if (isset(exif_read_data($img)["Artist"]))
-                            $author = exif_read_data($img)["Artist"];
+                            $description = '';
+                            if (isset(exif_read_data($img)["ImageDescription"]))
+                                $description = exif_read_data($img)["ImageDescription"];
 
-                        array_push($dataArray, array(
-                            "file" => exif_read_data($img)["FileName"],
-                            "description" => $description,
-                            "comment" => $comment,
-                            "author" => $author,
-                            "width" => exif_read_data($img)["ExifImageWidth"],
-                            "height" => exif_read_data($img)["ExifImageLength"],
-                            "Html" => "<img src=\"/img/" . exif_read_data($img)["FileName"] . "\" alt=\"" . exif_read_data($img)["FileName"] . "\"width=400 height=auto>"
-                        ));
+                            $comment = '';
+                            if (isset(exif_read_data($img)["UserComment"]))
+                                $comment = exif_read_data($img)["UserComment"];
 
+                            $author = '';
+                            if (isset(exif_read_data($img)["Artist"]))
+                                $author = exif_read_data($img)["Artist"];
+
+                            array_push($dataArray, array(
+                                "file" => exif_read_data($img)["FileName"],
+                                "description" => $description,
+                                "comment" => $comment,
+                                "author" => $author,
+                                "width" => exif_read_data($img)["ExifImageWidth"],
+                                "height" => exif_read_data($img)["ExifImageLength"],
+                                "Html" => "<img src=\"/img/" . exif_read_data($img)["FileName"] . "\" alt=\"" . exif_read_data($img)["FileName"] . "\"width=150 height=auto>"
+                            ));
+
+                        }
                     }
                 }
             }
         }
 
-        if ($name != null && count($dataArray) == 0)
+        if ($args != null && count($dataArray) == 0)
             return $this->response->withStatus(400);
 
-        $image = $this->response->withStringBody(json_encode($dataArray))->withType("application/json");
-        $this->set(compact('image'));
         $this->set(compact('dataArray'));
     }
 
