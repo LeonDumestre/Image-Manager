855ef46c17d7a63cc7bd963598afff9b9de871ab
Léon DUMESTRE <leon.dumestre@etu.univ-lyon1.fr>
2022-02-13 20:14:56 +0100

    Optimisation du code

--- src/Controller/ImagesController.php
+++ src/Controller/ImagesController.php
@@ -1,22 +1,18 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
 
-use phpDocumentor\Reflection\Types\Array_;
-
 class ImagesController extends AppController
 {
-
     public function index()
     {
-        $dataArray = array();
-        foreach (glob(WWW_ROOT.'img/*.jpg') as $img)
-        {
+        $dataArray = [];
+        foreach (glob(WWW_ROOT . 'img/*.jpg') as $img) {
             array_push($dataArray, $img);
         }
-         return $this->response->withStringBody(json_encode($dataArray))->withType("application/json");
+         return $this->response->withStringBody(json_encode($dataArray))->withType('application/json');
     }
-
 
     public function addAll()
     {
@@ -27,23 +23,24 @@ class ImagesController extends AppController
         if (count($images) == 0) {
             foreach (glob(WWW_ROOT . 'img/*.jpg') as $img) {
                 $image = $this->Images->newEmptyEntity();
-                $image->name = exif_read_data($img)["FileName"];
+                $image->name = exif_read_data($img)['FileName'];
 
                 $description = '';
-                if (isset(exif_read_data($img)["ImageDescription"]))
-                    $description = exif_read_data($img)["ImageDescription"];
+                if (isset(exif_read_data($img)['ImageDescription'])) {
+                    $description = exif_read_data($img)['ImageDescription'];
+                }
                 $image->description = $description;
 
-                $image->width = exif_read_data($img)["ExifImageWidth"];
-                $image->height = exif_read_data($img)["ExifImageLength"];
+                $image->width = exif_read_data($img)['ExifImageWidth'];
+                $image->height = exif_read_data($img)['ExifImageLength'];
 
                 $this->Images->save($image);
             }
-            if (count($images) > 0)
-                $this->Flash->success("La base de données a été remplie avec succès !");
+            if (count($images) > 0) {
+                $this->Flash->success('La base de données a été remplie avec succès !');
+            }
         }
     }
-
 
     public function add()
     {
@@ -52,43 +49,43 @@ class ImagesController extends AppController
 
         $data = $this->getRequest()->getData();
         if (!empty($data)) {
-            $fileData = $data["File"];
+            $fileData = $data['File'];
 
-            if(count($this->Images->find()->where(['Name LIKE' => $data['Name'].'.jpg'])->toArray()) > 0)
+            if (count($this->Images->find()->where(['Name LIKE' => $data['Name'] . '.jpg'])->toArray()) > 0) {
                 $this->Flash->error('Ce nom est déjà utilisé !');
-            else {
-                $image->name = $data["Name"] . ".jpg";
-                $image->description = $data["Description"];
+            } else {
+                $image->name = $data['Name'] . '.jpg';
+                $image->description = $data['Description'];
 
-                $fileData->moveTo(WWW_ROOT . "img/" . $image->name);
+                $fileData->moveTo(WWW_ROOT . 'img/' . $image->name);
 
-                $imageSize = getimagesize(WWW_ROOT . "img/" . $image->name);
+                $imageSize = getimagesize(WWW_ROOT . 'img/' . $image->name);
                 $image->width = $imageSize[0];
                 $image->height = $imageSize[1];
 
                 if ($this->Images->save($image)) {
                     $this->Flash->success("L'image a été ajoutée avec succès !");
                     return $this->redirect(['controller' => 'Images', 'action' => 'listing']);
-                } else
+                } else {
                     $this->Flash->error("Mince ! L'image n'a pas pu être ajoutée...");
+                }
             }
         }
     }
 
-
     public function delete($id)
     {
-        $this->getRequest()->allowMethod("post");
+        $this->getRequest()->allowMethod('post');
 
         $image = $this->Images->get($id);
-        if ($this->Images->delete($image))
+        if ($this->Images->delete($image)) {
             $this->Flash->success("L'image a été supprimée avec succès !");
-        else
+        } else {
             $this->Flash->error("Mince ! L'image n'a pas pu être supprimée...");
+        }
 
         return $this->redirect($this->referer());
     }
-
 
     public function listing($args = null)
     {
@@ -96,18 +93,20 @@ class ImagesController extends AppController
         $request = $this->getRequest()->getQuery();
 
         $page = 1;
-        if (isset($request["page"]))
-            $page = $request["page"];
+        if (isset($request['page'])) {
+            $page = $request['page'];
+        }
 
         $limit = 12;
-        if (isset($request["limit"]) && $request["limit"] < $limit)
-            $limit = $request["limit"];
+        if (isset($request['limit']) && $request['limit'] < $limit) {
+            $limit = $request['limit'];
+        }
 
         $info = false;
-        $name = "%";
-        if (isset($request["name"]))
-            $name = "%" . $request["name"] . "%";
-        else if ($args != null) {
+        $name = '%';
+        if (isset($request['name']))
+            $name = '%' . $request['name'] . '%';
+        elseif ($args != null) {
             $name = $args;
             $info = true;
         }
@@ -116,16 +115,18 @@ class ImagesController extends AppController
             ->find()
             ->where(['name LIKE' => $name])
             ->limit($limit)
-            ->offset(($page-1)*$limit)
+            ->offset(($page - 1) * $limit)
             ->order(['name' => 'ASC'])
             ->toArray();
 
-        if (count($images) == 0)
+        if (count($images) == 0) {
             return $this->response->withStatus(400);
+        }
 
         $allImages = $this->Images->find()->toArray();
-        if ($limit != 0)
+        if ($limit != 0) {
             $maxPage = count($allImages) / $limit;
+        }
 
         $this->set(compact('images'));
         $this->set(compact('limit'));
@@ -133,5 +134,4 @@ class ImagesController extends AppController
         $this->set(compact('maxPage'));
         $this->set(compact('info'));
     }
-
 }
